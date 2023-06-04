f4d56c8b9b4e51ee9b719654c0ed87ae8029af61
Pascal <ro020701@gmail.com>
2023-06-04 23:07:36 +0800

    2034.06.04 新增部分註解

--- pcTempCheck.py
+++ pcTempCheck.py
@@ -3,10 +3,10 @@ import pyautogui
 import pynput
 import os
 
-keyIn = ''
-count = 0
-enabled = True
-time_check = 0
+keyIn = ''  # 輸入的鍵盤指令
+count = 0  # 指令輸入次數
+enabled = True  # 致能flag
+time_check = 0  # 吃食物記錄
 
 
 def key_action():
@@ -14,20 +14,21 @@ def key_action():
     global keyIn
     global time_check
 
+    # 間隔一秒左鍵及定時吃食物
     if keyIn == 'z':
         if count == 1:
             localtime = time.localtime()
             mStr = time.strftime("%M", localtime)
             mNumber = int(mStr)
 
-            # 每十分鐘吃一次食物
+            # 每幾分鐘吃一次食物
             if mNumber % 5 == 0 and time_check == 0:
-                pyautogui.scroll(150)
+                pyautogui.scroll(150)  # 滑鼠滾輪往上
                 pyautogui.mouseDown(button='right')
                 time.sleep(3)
                 pyautogui.mouseUp(button='right')
                 time.sleep(1)
-                pyautogui.scroll(-150)
+                pyautogui.scroll(-150)  # 滑鼠滾輪往下
                 time_check = time_check + 1
 
             elif mNumber % 5 != 0 and time_check != 0:
@@ -43,6 +44,7 @@ def key_action():
             count = 0
             time_check = 0
 
+    # 持續左鍵
     elif keyIn == 'x':
         if count == 1:
             pyautogui.mouseDown(button='left')
@@ -75,11 +77,13 @@ def key_set(event):
             count = count + 1
             time.sleep(0.3)
 
+        # 關閉所有功能並開啟/失效指令輸入
         elif hasattr(key_event.key, 'name') and key_event.key.name == 'f12':
             keyIn = ''
             count = 0
             time_check = 0
             pyautogui.mouseUp(button='left')
+            pyautogui.mouseUp(button='right')
             enabled = not enabled
             time.sleep(0.3)
 
@@ -94,7 +98,7 @@ def key_set(event):
 
 # # # # # # # # # # #
 if __name__ == '__main__':
-    print("Z   : 間隔一秒左鍵")
+    print("Z   : 間隔一秒左鍵(會定期吃食物)")
     print("X   : 長壓左鍵")
     print("F12 : 失效/開啟功能")
     print("方向鍵右關閉程式")
